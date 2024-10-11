08b3118641b801bc407f7a5a4a2035afe8eb03fa
Лиля Юмакаева <lily@yoomoney.ru>
2024-10-11 14:36:32 +0300

    home work for lesson 8

---
+++ HomeWorkLesson8.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+enum AppleDevice {
+    case iPad
+    case iPhone
+    case macBook
+    case appleWatch
+    case iMac
+    
+    var screenSize: CGSize {
+        switch self {
+        case .appleWatch: return CGSize(width: 39, height: 46)
+        case .iPad: return CGSize(width: 174.1, height: 250.6)
+        case .iPhone: return CGSize(width: 77.6, height: 163)
+        case .macBook: return CGSize(width: 304.1, height: 212.4 ) 
+        case .iMac: return CGSize(width: 547, height: 461)
+        }
+    }
+    
+    var isPad: Bool {
+        switch self {
+        case .appleWatch: return false
+        case .iPad: return true
+        case .iPhone: return false
+        case .macBook: return false
+        case .iMac: return false
+        }
+    }
+}
+
+extension AppleDevice: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .appleWatch: return "Apple Watch"
+        case .iPad: return "iPad"
+        case .iPhone: return "iPhone"
+        case .macBook: return "MacBook"
+        case .iMac: return "iPod"
+        }
+    }
+}
+
+let myFirstAppleDevice = AppleDevice.macBook
+print("My first apple device was \(myFirstAppleDevice.description)")
+print("My first apple device screen size was \(myFirstAppleDevice.screenSize.width) x \(myFirstAppleDevice.screenSize.height)")
+print("Was my first apple device iPad? \(myFirstAppleDevice.isPad)")
